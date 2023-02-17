6826f98f9c374e380b159752a2e337ebb5277696
Gabriel Rozendo <79455923+rozendox@users.noreply.github.com>
2023-02-17 11:13:43 -0300

    Create multi.java

---
+++ multi.java
@@ -0,0 +1,8 @@
+public class MultiplicaVariaveis {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+        int multiplicacao = a * b;
+        System.out.println("A multiplicação de a por b é: " + multiplicacao);
+    }
+}
