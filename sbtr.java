8544206f8e7d8bb7bba374427797bee279725bac
Gabriel Rozendo <79455923+rozendox@users.noreply.github.com>
2023-02-17 11:12:05 -0300

    Create sbtr.java

---
+++ sbtr.java
@@ -0,0 +1,8 @@
+public class SubtraiVariaveis {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int subtracao = a - b;
+        System.out.println("A subtração de a e b é: " + subtracao);
+    }
+}
