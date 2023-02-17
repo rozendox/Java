0678399b65bf79f29db5db03dbbf525f3e84318f
Gabriel Rozendo <79455923+rozendox@users.noreply.github.com>
2023-02-17 11:13:00 -0300

    Create div.java

---
+++ div.java
@@ -0,0 +1,8 @@
+public class DivideVariaveis {
+    public static void main(String[] args) {
+        double a = 10;
+        double b = 2;
+        double divisao = a / b;
+        System.out.println("A divisão de a por b é: " + divisao);
+    }
+}
